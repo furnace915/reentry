1abfd91533398def02575755a9a3b41750c97313
Brent Velandra <brent.velandra@liminalarc.co>
2026-07-15 23:22:46 -0400

    update build.gradle and add test assertion

--- src/test/java/com/example/reentry/ReEntryApplicationTests.java
+++ src/test/java/com/example/reentry/ReEntryApplicationTests.java
@@ -1,12 +1,18 @@
 package com.example.reentry;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ReEntryApplicationTests {
 
+    @Autowired
+    private org.springframework.context.ApplicationContext context;
+
     @Test
     void shouldLoadApplicationContext() {
+        Assertions.assertNotNull(context, "The application context should have loaded.");
     }
 }
